9a0658da013c8e2dc7d048bbda9e0ec3daa50290
EA - Cauã Carvalho De Oliveira <caua_carvalho01@educadventista.org>
2023-09-25 14:57:22 -0300

    Aula 2

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const path = require("path")
+
 
 const app = express()
 
+const caminho = path.join(__dirname,'templates')
+
 app.listen(3000,() => {
+    
     console.log("Server Running in port:3000")
 })
 
 app.get('/',(req,res)=>{
-    res.send("Cauã Carvalho")
+    res.sendFile(`${caminho}/index.html`)
 })
